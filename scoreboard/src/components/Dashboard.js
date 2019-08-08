f5f32249aa3b8848bffd8e4752c9998ae64465b8
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-08-08 14:50:31 -0700

    added display and dashboard components

---
+++ scoreboard/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+const Dashboard = () => {
+
+    return (
+        <div className = 'dashboard'>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>Hit</button>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ scoreboard/src/components/Display.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Display = () => {
+
+    return (
+        <div className = 'scoreboard'>
+            <h2>Balls: </h2>
+            <h2>Strikes: </h2>
+        </div>
+    )
+}
+
+export default Display;
