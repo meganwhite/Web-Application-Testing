ba42512868a7682ec505b071e91c82634c962f58
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-08-09 12:13:25 -0700

    added tests

--- scoreboard/src/App.test.js
+++ scoreboard/src/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
+import App from './App'
 import {strike} from "./App"
+import ReactDOM from 'react-dom'
+import {render} from '@testing-library/react';
 
 afterEach(rtl.cleanup);
 
@@ -12,6 +15,19 @@ it('renders without crashing', () => {
 });
 
 describe("App.js",() => {
+  it('renders without crashing using ReactDOM', () => {
+    const wrapper = rtl.render(
+      <span className="greet">hello world</span>
+    );
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div)
+  });
+
+  it('renders without crashing', () => {
+    render(<App/>)
+
+  });
   describe("strike()",() => {
     it("should return 0 if strikes is already 2 or add 1 to strikes if strikes is less than 2", () => {
       //expect
